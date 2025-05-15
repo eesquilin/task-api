d4e5587a36aae2bdf7c4f3d8b7fa9f4aa399159f
eesquilinv <e.esquilinv@gmail.com>
2025-05-15 16:22:10 -0400

    created shouldReturnTaskById() method to test getTaskbyId() method.

--- src/test/java/com/eesquilinv/task_api/controller/TaskControllerIntegrationTest.java
+++ src/test/java/com/eesquilinv/task_api/controller/TaskControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -15,8 +16,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TaskControllerIntegrationTest {
 
+    @LocalServerPort
+    private int port;
+
     @Autowired
     TestRestTemplate testRestTemplate;
+
+    private String getUrl(){
+        return "localhost:" + port;
+    }
 
     @Test
     public void shouldCreateNewTask(){
@@ -34,6 +42,23 @@ public class TaskControllerIntegrationTest {
         assertEquals("2025-05-20", response.getBody().getDueDate());
         assertEquals("This is a test task.", response.getBody().getDescription());
         assertEquals(TaskStatus.TODO, response.getBody().getTaskStatus());
+
+    }
+
+    public void shouldReturnTaskById(Long Id){
+        Task newTask = new Task();
+        newTask.setTitle("Test Task");
+        newTask.setDueDate("2025-05-20");
+        newTask.setDescription("This test retrieves task by id.");
+        newTask.setTaskStatus(TaskStatus.INPROGRESS);
+
+        //Post the test-newTask object into the test-entity.
+        ResponseEntity<Task> response = testRestTemplate.postForEntity("/tasks", newTask, Task.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+
+
 
     }
 }
